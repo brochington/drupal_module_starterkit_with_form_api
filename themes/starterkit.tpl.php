c7943022cb20a88be1a88670ff7997bbc8aa2d04
Stilley, Broch <Broch.Stilley@nfl.com>
2014-07-14 16:46:26 -0700

    Initial Commit!

---
+++ themes/starterkit.tpl.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * @file
+ * Template file for starterkit. List available variables below:
+ *
+ * Available variables:
+ * - $test_var: just a simple test variable...
+ */
+ ?>
+
+ <div>
+ 	<?php print $test_var ?>
+ </div>
